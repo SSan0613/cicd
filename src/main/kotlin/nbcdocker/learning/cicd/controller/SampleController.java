e1c693e9fc8fad4b6d3ad7e1d97f7ff202b4e500
SSan0613 <sangcu21@naver.com>
2025-02-13 14:29:23 +0900

    test2

---
+++ src/main/kotlin/nbcdocker/learning/cicd/controller/SampleController.java
@@ -0,0 +1,10 @@
+package nbcdocker.learning.cicd.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SampleController {
+    public void test2() {
+        System.out.println("hello");
+    }
+}
